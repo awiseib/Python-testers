76c2522f662e7c96c0ec44e232f2f68ce4ebbbe8
Andrew W <awise@interactivebrokers.com>
2023-04-21 08:55:49 -0500

    Updated token and notes for ease of use

--- FlexWebQuery.py
+++ FlexWebQuery.py
@@ -4,35 +4,34 @@ import csv
 import xml.etree.ElementTree as ET
 
 # Update these to YOUR environment.
-csvPath = 'D:\\Downloads\\Trades.csv'
+csvPath = 'D:\\Downloads\\MINT.csv'
 
 # Flex Web Serivce is only availble for LIVE accounts.
 # This example uses the trust test account.
 # Because the trust account is Read-Only, no contents will show besides the header
 
 # 1. Build your flex query in Client Portal
-# 2. Right click the page, click "Inspect"
-# 3. Go to Network tab
-# 4. You will see a Data response -> In the "entries" section, you will find the "queryId" field with the corresponding value
+# 2. Click the Info button to the left of your Flex Query
+# 3. Copy the "Query ID" under Activity Flex Query Details
 
 requestBase = "https://www.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?"
-token = "t=656530010732398240553724" # Valid 2023-03-01, 10:13:43 EST — 2024-01-31, 10:13:43 EST
-queryId = "&q=771568"
+token = "t=220638997034176155165000" # Valid 2023-03-01, 10:13:43 EST — 2024-01-31, 10:13:43 EST
+queryId = "&q=776579"
 version = "&v=3"
 
-# 5. Combine requestUrl, queryId, tokenId, and include version 3
+# 4. Combine requestUrl, queryId, tokenId, and include version 3
 requestUrl = "".join([requestBase, token, queryId, version])
 # Example: https://www.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?t=174034019902345466159208&q=771568&v=3
 
-# 6. Create a GET request for that URL
+# 5. Create a GET request for that URL
 flexReq = requests.get(url=requestUrl)
 
 # Read XML
 tree = ET.ElementTree(ET.fromstring(flexReq.text))
 root = tree.getroot()
 
-# 7. If <Status> received is "Success", you have begun the process
-# 8. You will need to use the <Url> and <ReferenceCode> value to retrieve the value
+# 6. If <Status> received is "Success", you have begun the process
+# 7. You will need to use the <Url> and <ReferenceCode> value to retrieve the value
 
 for child in root:
     if child.tag == "Status":
@@ -48,7 +47,7 @@ for child in root:
         print(child.tag, ":", child.text)
 
 
-# 9. Combine your new url, reference code, and your previous token and version number
+# 8. Combine your new url, reference code, and your previous token and version number
 
 receiveUrl = "".join([receiveBase, "?",token, refCode, version])
 # Example: https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?t=174034019902345466159208&q=5773202049&v=3
@@ -57,10 +56,10 @@ receiveUrl = "".join([receiveBase, "?",token, refCode, version])
 print("Hold for Request.")
 time.sleep(10)
 
-# 10. Generate a GET request for the new URL
+# 9. Generate a GET request for the new URL
 receiveUrl = requests.get(url=receiveUrl, allow_redirects=True)
 
-# 11. CSV value returned
+# 10. CSV value returned
 open(csvPath, 'wb').write(receiveUrl.content)
 
 
