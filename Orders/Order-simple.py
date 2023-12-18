cb41ac6657791b5a22fa7a2e8a7d401fd7256c41
Andrew W <awise@interactivebrokers.com>
2023-12-18 13:17:13 -0600

    outsideRth

--- Orders/Order-simple.py
+++ Orders/Order-simple.py
@@ -1,5 +1,7 @@
 from ibapi.client import *
 from ibapi.wrapper import *
+import time
+port=7496
 
 class TestApp(EClient, EWrapper):
     def __init__(self):
@@ -8,35 +10,35 @@ class TestApp(EClient, EWrapper):
     def nextValidId(self, orderId: OrderId):
 
         mycontract = Contract()
-        # mycontract.conId = 265598
-
-        mycontract.symbol = "AAPL"
-        mycontract.secType = "STK"    
+        mycontract.conId = 265598 # AAPL Conid
         mycontract.exchange = "SMART"
-        mycontract.currency = "USD"
 
         myorder = Order()
-        myorder.orderId = orderId
         myorder.action = "BUY"
         myorder.tif = "GTC"
         myorder.orderType = "LMT"
-        myorder.lmtPrice = 165
+        myorder.lmtPrice = 180
+        myorder.outsideRth = True
         myorder.totalQuantity = 5
 
-        self.placeOrder(myorder.orderId, mycontract, myorder)
+        self.placeOrder(orderId, mycontract, myorder)
 
     def openOrder(self, orderId: OrderId, contract: Contract, order: Order, orderState: OrderState):
-        print(f"openOrder. orderId: {orderId}, contract: {contract}, order: {order}")
-        print(f"orderState: {orderState.commission}")
+        print(f"openOrder. orderId: {orderId}, contract: {contract}, order: {order}") 
         print(f"Maintenance Margin: {orderState.maintMarginAfter}, {orderState.maintMarginBefore}, {orderState.maintMarginChange}")
         print(f"Initial Margin: {orderState.initMarginAfter}, {orderState.initMarginBefore}, {orderState.initMarginChange}")
+
 
     def orderStatus(self, orderId: OrderId, status: str, filled: Decimal, remaining: Decimal, avgFillPrice: float, permId: int, parentId: int, lastFillPrice: float, clientId: int, whyHeld: str, mktCapPrice: float):
         print(f"orderId: {orderId}, status: {status}, filled: {filled}, remaining: {remaining}, avgFillPrice: {avgFillPrice}, permId: {permId}, parentId: {parentId}, lastFillPrice: {lastFillPrice}, clientId: {clientId}, whyHeld: {whyHeld}, mktCapPrice: {mktCapPrice}")
 
+
     def execDetails(self, reqId: int, contract: Contract, execution: Execution):
         print(f"reqId: {reqId}, contract: {contract}, execution: {execution}")
 
+    def error(self, reqId: TickerId, errorCode: int, errorString: str, advancedOrderRejectJson=""):
+        print("Error", reqId, errorCode, errorString, advancedOrderRejectJson)
+
 app = TestApp()
-app.connect("127.0.0.1", 7496, 100)
+app.connect("127.0.0.1", port, 0)
 app.run()
