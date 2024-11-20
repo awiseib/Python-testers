fff8841c9458199012b7c0710c4996f3bc0ead69
awiseib <awise@interactivebrokers.com>
2024-11-20 12:14:33 -0600

    WshEvent Update

--- Wall_Street_Horizons/WSH-Event.py
+++ Wall_Street_Horizons/WSH-Event.py
@@ -10,15 +10,17 @@ class TestApp(EClient, EWrapper):
         EClient.__init__(self, self)
 
     def nextValidId(self, orderId: OrderId):
+
+        # Either ConID or Filter may be passed, but not both.
         eventData = WshEventData()
-        eventData.conId = 2147483647
-        eventData.startDate = "20240425"
-        eventData.endDate = "20240509"
-        # eventData.fillCompetitors = False
-        # eventData.fillPortfolio = False
-        # eventData.fillWatchlist = False
+        # eventData.conId = 265598
+        eventData.startDate = "20241109"
+        eventData.endDate = "20241120"
+        eventData.fillCompetitors = False
+        eventData.fillPortfolio = False
+        eventData.fillWatchlist = False
         eventData.totalLimit = 100
-        eventData.filter = '{"country": "All","watchlist":["493411337", "136348477", "14183", "75259168", "250083553", "35151816"], "splits":"true","wshe_splits":"true","wshe_idx":"true","wshe_eps":"true","corporate_earnings":"true","wshe_merg_acq":"true","wshe_ed":"true","wshe_divsr":"true","wshe_fq":"true","DivExDates":"true","wshe_bybkmod":"true","wshe_spinoffs":"true","wshe_bybk":"true","ipo":"true"}'
+        eventData.filter = '{"country": "All","watchlist":["265598"], "wshe_fq":"true"}'
 
         """
         Filters pulled from wshMetaData. Look for section header in response to find filter name.
@@ -27,16 +29,9 @@ class TestApp(EClient, EWrapper):
         wshe_bod    ==  Board of Directors
         wshe_ed     ==  Earnings Dates
         wshe_div    ==  Dividend Dates
-        wshe_eps    ==   
-        wshe_cc     ==
-        wshe_option ==
-        wshe_sec    == 
         wshe_qe     ==  Quarterly Earnings
-        wshe_ic     ==
-        wshe_sh     == 
         """
-        # Please note, the third param for self.serverVersion is only necessary for the TWS API Latest release.
-        # This would be anything beyond 10.19.
+        # Please note, the third param for self.serverVersion is not necessary for API releases prior to 10.20.
         self.reqWshEventData(orderId, eventData, self.serverVersion())
 
     def wshEventData(self, reqId: int, dataJson: str):
