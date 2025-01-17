f489d0046033769a9d9991fa35d0563dd83304d7
awiseib <108894870+awiseib@users.noreply.github.com>
2025-01-17 15:07:13 -0600

    Update Order-OCA.py

--- Orders/Order-OCA.py
+++ Orders/Order-OCA.py
@@ -1,9 +1,14 @@
 from decimal import Decimal
-from datetime import datetime
 from ibapi.client import *
+from ibapi.common import OrderId
+from ibapi.contract import Contract
+from ibapi.order import Order
+from ibapi.order_state import OrderState
 from ibapi.wrapper import *
-from ibapi.contract import *
 from ibapi.order_state import *
+from datetime import datetime
+from threading import Thread
+import time
 
 port = 7496
 
@@ -11,134 +16,80 @@ port = 7496
 class TestApp(EClient, EWrapper):
     def __init__(self):
         EClient.__init__(self, self)
+        self.orderId = 0
 
     def nextValidId(self, orderId: OrderId):
         print(f"nextValidId. orderId={orderId}")
+        self.orderId = orderId
+    
+    def nextOid(self):
+        self.orderId += 1
+        return self.orderId
 
-        parentContract = Contract() 
-        parentContract.conId = 265598 # AAPL Options
-        parentContract.exchange = "SMART"
-        parentContract.currency = "USD"
-
-        child1 = Contract() 
-        child1.conId = 265598 # AAPL Options
-        child1.exchange = "SMART"
-        child1.currency = "USD"
-
-        child2 = Contract() 
-        child2.conId = 265598 # AAPL Options
-        child2.exchange = "SMART"
-        child2.currency = "USD"
-
-        parentOrder = Order()
-        parentOrder.orderId = orderId
-        parentOrder.action = "BUY"
-        # parentOrder.orderType = "LMT"
-        # parentOrder.tif = "OPG"
-        parentOrder.orderType = "LOC"
-        parentOrder.tif = "DAY"
-        parentOrder.lmtPrice = "169"
-        parentOrder.totalQuantity = 1
-        parentOrder.ocaGroup = "TestOCA_", orderId
-        parentOrder.ocaType = 1
-        parentOrder.transmit = True
-
-        self.placeOrder(orderId, parentContract, parentOrder)
-
-        chilldO1 = Order()
-        chilldO1.orderId = orderId + 1
-        chilldO1.action = "SELL"
-        # chilldO1.orderType = "LMT"
-        # chilldO1.tif = "OPG"
-        chilldO1.orderType = "LOC"
-        chilldO1.tif = "DAY"
-        chilldO1.lmtPrice = 180
-        chilldO1.totalQuantity = 1
-        chilldO1.ocaGroup = "TestOCA_", orderId
-        chilldO1.ocaType = 1
-        chilldO1.transmit = True
-
-        self.placeOrder(chilldO1.orderId, child1, chilldO1)
-
-
-        chilldO2 = Order()
-        chilldO2.orderId = orderId + 2
-        chilldO2.action = "BUY"
-        chilldO2.orderType = "MKT"
-        chilldO2.totalQuantity = 1
-        chilldO2.ocaGroup = "TestOCA_", orderId
-        chilldO2.ocaType = 3
-        chilldO2.transmit = True
-
-        # self.placeOrder(chilldO2.orderId, child2, chilldO2)
-
-
-    def openOrder(
-        self,
-        orderId: OrderId,
-        contract: Contract,
-        order: Order,
-        orderState: OrderState,
-    ):
-        print(
-            "openOrder.",
-            f"orderId:{orderId}",
-            f"contract:{contract}",
-            f"order:{order}",
-            f"orderState:{orderState}",
-            # "\n",
-            # f"Margin Values: \n",
-            # f"initMarginBefore: {orderState.initMarginBefore}; initMarginAfter: {orderState.initMarginAfter};initMarginChange: {orderState.initMarginChange}; \n",
-            # f"maintMarginBefore: {orderState.maintMarginBefore }; maintMarginAfter: {orderState.maintMarginAfter};maintMarginChange: {orderState.maintMarginChange}; \n",
-        )
-
-    def orderStatus(
-        self,
-        orderId: OrderId,
-        status: str,
-        filled: Decimal,
-        remaining: Decimal,
-        avgFillPrice: float,
-        permId: int,
-        parentId: int,
-        lastFillPrice: float,
-        clientId: int,
-        whyHeld: str,
-        mktCapPrice: float,
-    ):
-        print(
-            "orderStatus.",
-            f"orderId:{orderId}",
-            f"status:{status}",
-            f"filled:{filled}",
-            f"remaining:{remaining}",
-            f"avgFillPrice:{avgFillPrice}",
-            # f"permId:{permId}",
-            f"parentId:{parentId}",
-            f"lastFillPrice:{lastFillPrice}",
-            # f"clientId:{clientId}",
-            # f"whyHeld:{whyHeld}",
-            # f"mktCapPrice:{mktCapPrice}",
-        )
-
-    def error(
-        self,
-        reqId: TickerId,
-        errorCode: int,
-        errorString: str,
-        advancedOrderRejectJson="",
-    ):
-        print(
-            datetime.now().strftime("%H:%M:%S.%f")[:-3],
-            "error.",
-            f"reqId:{reqId}",
-            f"errorCode:{errorCode}",
-            f"errorString:{errorString}",
-            f"advancedOrderRejectJson:{advancedOrderRejectJson}",
-        )
-
+    def openOrder(self, orderId: int, contract: Contract, order: Order, orderState: OrderState):
+        print(f"openOrder. orderId: {orderId}, contract: {contract.symbol}, order: {order}, orderState: {orderState.status}")
+    
+    def orderStatus(self, orderId: OrderId, status: str, filled: Decimal, remaining: Decimal, avgFillPrice: float, permId: OrderId, parentId: OrderId, lastFillPrice: float, clientId: OrderId, whyHeld: str, mktCapPrice: float):
+        print(f"orderStatus. OrderId: {orderId}, Status: {status}")
 
 
 app = TestApp()
 app.connect("127.0.0.1", port, 1001)
-app.run()
+time.sleep(3)
+Thread(target=app.run).start()
+app.nextValidId(-1)
+time.sleep(1)
+
+### Leg for AAPL
+leg1_contract = Contract() 
+leg1_contract.conId = 265598
+leg1_contract.exchange = "SMART"
+
+leg1_order = Order()
+leg1_order.tif = "DAY"
+leg1_order.orderId = app.nextOid()
+leg1_order.action = "BUY"
+leg1_order.orderType = "LMT"
+leg1_order.lmtPrice = 250
+leg1_order.totalQuantity = 1
+leg1_order.ocaGroup = "TestOCA_", leg1_order.orderId
+leg1_order.ocaType = 1
+leg1_order.transmit = True
+
+app.placeOrder(leg1_order.orderId, leg1_contract, leg1_order)
+
+## Leg for IBM
+leg2_contract = Contract() 
+leg2_contract.conId = 8314
+leg2_contract.exchange = "SMART"
+
+leg2_order = Order()
+leg2_order.tif = "GTC"
+leg2_order.orderId = app.nextOid()
+leg2_order.action = "SELL"
+leg2_order.orderType = "LMT"
+leg2_order.lmtPrice = 229
+leg2_order.totalQuantity = 1
+leg2_order.ocaGroup = "TestOCA_", leg1_order.orderId
+leg2_order.ocaType = 1
+leg2_order.transmit = True
+
+app.placeOrder(leg2_order.orderId, leg2_contract, leg2_order)
+
+## Leg for KO
+leg3_contract = Contract() 
+leg3_contract.conId = 8894
+leg3_contract.exchange = "SMART"
+
+leg3_order = Order()
+leg3_order.tif = "GTC"
+leg3_order.orderId = app.nextOid()
+leg3_order.action = "BUY"
+leg3_order.orderType = "LMT"
+leg3_order.totalQuantity = 62
+leg3_order.lmtPrice = 10
+leg3_order.ocaGroup = "TestOCA_", leg1_order.orderId
+leg3_order.ocaType = 1
+leg3_order.transmit = True
+
+app.placeOrder(leg3_order.orderId, leg3_contract, leg3_order)
