8c52c256cdf7866e239a7abdf92c3a818a3791ff
awiseib <108894870+awiseib@users.noreply.github.com>
2025-03-10 13:16:15 -0500

    Update reqExecutions.py
    Updated to reflect execFilter parameters to include the commissionAndFeesReport function

--- Account/reqExecutions.py
+++ Account/reqExecutions.py
@@ -7,23 +7,34 @@ class TestApp(EClient, EWrapper):
 
     def __init__(self):
         EClient.__init__(self, self)
+        self.orderId = 0
+        self.exec_and_comms = {}
 
     def nextValidId(self, orderId: OrderId):
 
-        exec_filter = ExecutionFilter()
+        execFilter = ExecutionFilter()
+        execFilter.acctCode = ""
+        execFilter.time = ""
+        execFilter.exchange = ""
+        execFilter.secType = ""
+        execFilter.side = ""
+        execFilter.symbol = ""
+        execFilter.clientId = ""
 
-        self.reqExecutions(
-            orderId,
-            exec_filter
-        )
+        self.reqExecutions(self.orderId, execFilter)
+
 
     def execDetails(self, reqId: int, contract: Contract, execution: Execution):
-        print(reqId, contract, execution)
+        self.exec_and_comms[execution.execId] =  {"execDetails": execution}
+
+    def commissionAndFeesReport(self, commissionAndFeesReport: CommissionAndFeesReport):
+        self.exec_and_comms[commissionAndFeesReport.execId]["CommReport"] = commissionAndFeesReport
 
     def execDetailsEnd(self, reqId: int):
         print("execDetailsEnd.", reqId)
-        self.disconnect()
-
+        print(
+            "\n".join(f"{name}: {value}" for name, value in self.exec_and_comms.items())
+        )
 
 app = TestApp()
 app.connect("127.0.0.1", port, 0)
