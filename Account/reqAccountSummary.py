e5a1da8b0048c975f117e1ace62e543921f636e8
awiseib <108894870+awiseib@users.noreply.github.com>
2025-09-02 10:47:04 -0500

    Update reqAccountSummary.py
    Added Interactive Brokers standard header content

--- Account/reqAccountSummary.py
+++ Account/reqAccountSummary.py
@@ -1,3 +1,11 @@
+'''
+2002-2025: Use is subject to Interactive Brokers TWS API Non-Commercial License ("License") terms. 
+This License is NOT for anybody who is developing software applications that they wish to: (a) sell to third 
+party users for a fee, or (b) give to third party users to generate an indirect financial benefit (e.g., 
+commissions). If You wish to make a software application for the purposes described in the preceding 
+sentence then please contact Interactive Brokers
+'''
+
 from ibapi.client import *
 from ibapi.wrapper import *
 from ibapi.account_summary_tags import *
@@ -31,3 +39,4 @@ class TestApp(EClient, EWrapper):
 app = TestApp()
 app.connect("127.0.0.1", port, 0)
 app.run()
+
