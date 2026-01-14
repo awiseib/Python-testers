cc99eb8aad3e44a05a4267a59fd878615e8713d3
awiseib <108894870+awiseib@users.noreply.github.com>
2026-01-14 09:46:46 -0600

    Update Order-simple.py

--- Orders/Order-simple.py
+++ Orders/Order-simple.py
@@ -21,30 +21,20 @@ class TestApp(EClient, EWrapper):
         self.oid += 1
         return self.oid
 
-
     def openOrder(self, orderId: OrderId, contract: Contract, order: Order, orderState: OrderState):
         print(f"openOrder. orderId: {orderId}, contract: {contract}, order: {order}, orderState: {orderState.status}, Ref: {order.orderRef}")
-        # print(f"MidOffsetAtHalf: {order.midOffsetAtHalf}")
-        # attrs = vars(order)
-        # print("Order:")
-        # print(
-        #     "\n\t".join(f"{name}: {value}" for name, value in attrs.items())
-        # )
-        # print("Order State: ")
-        # print(
-        #     "\n\t".join(f"{name}: {value}" for name, value in vars(orderState).items())
-        # )
+        
     def openOrderEnd(self):
         print("End of open orders.")
 
-    # def orderStatus(self, orderId: TickerId, status: str, filled: Decimal, remaining: Decimal, avgFillPrice: float, permId: TickerId, parentId: TickerId, lastFillPrice: float, clientId: TickerId, whyHeld: str, mktCapPrice: float):
-    #     print(orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
+    def orderStatus(self, orderId: TickerId, status: str, filled: Decimal, remaining: Decimal, avgFillPrice: float, permId: TickerId, parentId: TickerId, lastFillPrice: float, clientId: TickerId, whyHeld: str, mktCapPrice: float):
+        print(orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
 
-    # def completedOrder(self, contract: Contract, order: Order, orderState: OrderState):
-    #     print(f"CompletedOrder. submitter: {order.submitter}")
+    def completedOrder(self, contract: Contract, order: Order, orderState: OrderState):
+        print(f"CompletedOrder. submitter: {order.submitter}")
 
-    # def execDetails(self, reqId: int, contract: Contract, execution: Execution):
-    #     print(f"reqId: {reqId}, contract: {contract}, execution: {execution}, submitter: {execution.submitter}")
+    def execDetails(self, reqId: int, contract: Contract, execution: Execution):
+        print(f"reqId: {reqId}, contract: {contract}, execution: {execution}, submitter: {execution.submitter}")
 
     def error(self, reqId: TickerId, errorTime: int, errorCode: int, errorString: str, advancedOrderRejectJson=""):
         print(f"Error., Time of Error: {errorTime}, Error Code: {errorCode}, Error Message: {errorString}")
@@ -57,7 +47,6 @@ if __name__ == "__main__":
     contract.symbol = "AAPL"
     contract.secType = "STK"
     contract.exchange = "SMART"
-    # contract.primaryExchange = "ARCA"
     contract.currency = "USD"
 
     myorder = Order()
@@ -65,19 +54,8 @@ if __name__ == "__main__":
     myorder.orderType = "LMT"
     myorder.lmtPrice = 262.90
     myorder.totalQuantity = 75
-    myorder.notHeld = False
-    
-    # myorder.tif = "IOC"
-    # myorder.ocaType=2
-    # myorder.cashQty = 100
-    # myorder.notHeld = True
-    # myorder.midOffsetAtHalf = 0.005000000001
-    # myorder.midOffsetAtWhole = 0.010
 
-    # myorder.account = "DF74648"
-    # myorder.faGroup = ""
-    # myorder.modelCode = "Test"
-
+    myorder.account = "DU74649"
 
     app = TestApp()
     app.connect("127.0.0.1", port, 0)
