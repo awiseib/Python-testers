228ebca93dc5bd9b02bc168460363a593e1dc091
awiseib <awise@interactivebrokers.com>
2026-02-10 12:31:21 -0600

    Updated Scale Orders.

--- Orders/Order-scaleOrder.py
+++ Orders/Order-scaleOrder.py
@@ -17,28 +17,26 @@ class TradingApp(EWrapper, EClient):
         order = Order()
         order.action = "BUY"
         order.orderType = "LMT"        
-        order.totalQuantity = 8000
+        order.totalQuantity = 1111          # TotalQuantity becomes Maximum Position for Scale Orders.
         order.lmtPrice = 17
         order.tif = 'DAY'
         
         #MAIN
-        order.scaleInitLevelSize  = 1000
-        order.scaleSubsLevelSize  = 200   #optional - After the initial fill of 2,000 shares switch to quantity = 100
-        order.scalePriceIncrement = 0.5   #must be > 0
-        order.scaleRandomPercent = False
+        order.scaleInitLevelSize  = 222     # Initial Component Size
+        order.scaleSubsLevelSize  = 33      # Subsequent Comp. Size
+        order.scalePriceIncrement = 10      # Price Increment
+        order.scaleRandomPercent = False    # Randomize size by +/- 55%
         
         #AUTO PRICE ADJUSTMENT
-        #order.scalePriceAdjustValue = .2 
-        #order.scalePriceAdjustInterval = 1   
+        order.scalePriceAdjustValue = 4     # Increase starting price by _
+        order.scalePriceAdjustInterval = 5  # Increase starting price every _
         
         #PROFIT TAKER
-        order.scaleProfitOffset = 2  
-        order.scaleAutoReset = False  
-        #order.scaleInitPosition = 2000   #seems optional
-        #order.scaleInitFillQty = 200     #seems optional
+        order.scaleProfitOffset = 6         # Profit Orders' Profit Offset
+        order.scaleAutoReset = False        # Profit Orders' Restore size after taking profit
+        order.scaleInitPosition = 7         # Profit Orders' Initial Position
+        order.scaleInitFillQty = 8          # Profit Orders' Filled Initial Component Size
         
-        #order.scaleTable = ""
-
         self.placeOrder(102, contract, order)
 
     
